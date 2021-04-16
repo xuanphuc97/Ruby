4464c30c9d7589680c52f2db8cca17ba01031f1b
xuanphuc97 <64199092+xuanphuc97@users.noreply.github.com>
2021-04-16 13:06:21 +0700

    Update Test.rb

--- LearnSyntax/Test.rb
+++ LearnSyntax/Test.rb
@@ -1,1 +1,18 @@
-puts "Hello World!"
+# ary = [  "fred", 10, 3.14, "This is a string", "last element", ]
+# ary.each do |i|
+#   puts i
+# end
+
+# ary = [  "fred", 10, 3.14, "This is a string", "last element", ]
+# for i in ary
+#   puts i
+# end
+
+# hsh = colors = { "red" => 0xf00, "green" => 0x0f0, "blue" => 0x00f }
+# hsh.each do |key, value|
+#   print key, " is ", value, "\n"
+# end
+
+(15..10-1).each do |i|
+  puts i
+end
